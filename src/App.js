5d4c4223a23659a508eb58a3a64bf586fcba27f0
topipulli <topi.pulli@myy.haaga-helia.fi>
2022-12-16 22:04:13 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  useQuery,
+    gql,
+  } from "@apollo/client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+  function App() {
+    const GET_COUNTRIES = gql`
+      query GetCountries {
+        countries{
+          code,
+          name,
+          continent{
+            name,
+          }
+          }
+    }
+    `;
+  
+    const {loading, error, data} = useQuery(GET_COUNTRIES);
+  
+    if (loading) return <p>Loading...</p>;
+    else if (error) return <p>Error...</p>
+    else {
+      return (
+        <div className="App">
+          <table>
+            <th>Country code</th><th>Country name</th><th>Continent</th>
+            <tbody>
+              {
+              data.countries.map((country, index) =>
+                <tr key={index}>
+                  <td>{country.code}</td>
+                  <td>{country.name}</td>
+                  <td>{country.continent.name}</td>
+                </tr>
+              )
+            }
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+  }
+  
 
 export default App;
+
+
+
--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/',
+  cache: new InMemoryCache()
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>  
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
